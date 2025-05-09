03f8a95b6ff612296eec238714ca40e0d6dae0b8
habil-GIC <habil@gic-indonesia.com>
2025-05-09 16:35:21 +0700

    fix(build): fix for deployment

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "@modules/auth/components"
+import { Suspense } from "react"
 
 export default function Auth() {
   return (
     <div className=" w-[300px] md:w-1/3 lg:w-1/3 rounded-2xl bg-white">
-      <LoginForm/>
+      <Suspense>
+        <LoginForm/>
+      </Suspense>
     </div>
   )
 }
